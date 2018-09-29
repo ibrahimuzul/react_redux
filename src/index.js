0cc6f7d83a408296f92ccaa2d3ff6337d8012a43
DESKTOP-HFP9RHC\ibrahim <ibrahim.uzul@gmail.com>
2018-09-29 17:57:50 +0300

    First Commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+import promise from 'redux-promise';
+
+//import App from './components/app';
+import reducers from './reducers';
+import PostsIndex from './components/posts_index';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+/*class Hello extends React.Component {
+  render() { return <div>Hello!</div> }
+}
+
+class Goodbye extends React.Component {
+  render() { return <div>Goodbye!</div> }
+}*/
+
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostsIndex}></Route>
+      </div>
+    </BrowserRouter>
+  </Provider>
+  , document.querySelector('.container'));
