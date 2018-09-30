df54838a30bf0f29029a849bf9a661e0042a739c
DESKTOP-HFP9RHC\ibrahim <ibrahim.uzul@gmail.com>
2018-09-30 13:46:43 +0300

    101 Caching Records

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPost } from '../actions'
 
 class PostsShow extends Component {
-    componentDidMount(){
-        const {id} = this.props.match.params;
-        this.props.fetchPost(id);
+    componentDidMount() {
+        //if (!this.props.post) {
+            const { id } = this.props.match.params;
+            this.props.fetchPost(id);
+        //}
     }
     render() {
-        const {post} =this.props;
+        const { post } = this.props;
 
-        if (!post){
+        if (!post) {
             return <div>Loading...</div>;
         }
 
         return (
             <div>
+                <Link to="/">Back To Index</Link>
                 <h3>{post.title}</h3>
                 <h6>Categories: {post.categories}</h6>
                 <p>{post.content}</p>
@@ -24,8 +28,8 @@ class PostsShow extends Component {
     }
 }
 
-function mapStateToProps({posts},ownProps){
-    return {post:posts[ownProps.match.params.id]};
+function mapStateToProps({ posts }, ownProps) {
+    return { post: posts[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps, {fetchPost})(PostsShow);
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
